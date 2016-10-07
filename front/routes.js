5a5c1c7f8e63401e638fbf07aa6aa2fbba16c21b
Vincent Chapron <vincent@chapron.io>
2016-10-07 22:38:15 +0200

    add simple react project

--- front/routes.js
+++ front/routes.js
@@ -4,8 +4,8 @@ const express = require('express');
 
 let routes = express.Router();
 
-routes.get('/', (req, res, next) => {
-	res.render('index');
+routes.get('/index.html', (req, res, next) => {
+    res.render('index', {title: 'react + api boilerplate'});
 });
 
 module.exports = routes;
