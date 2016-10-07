b6bcfeaa64e47be8226bdfff7a961107d7bc7403
Vincent Chapron <vincent@chapron.io>
2016-10-07 17:35:59 +0200

    add configuration to use ssl if certificate and privateKey are given

---
+++ configuration/server.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const fs = require('fs');
+
+module.exports = class ServerConfiguration {
+
+    constructor() {
+        let encoding = process.env.APP_ENCODING || 'utf-8';
+        let privateKeyFilePath = process.env.APP_PRIVATEKEY || 'privatekey.pem';
+        let certificateFilePath = process.env.APP_CERTIFICATE || 'cert.pem';
+
+        this.httpPort = process.env.APP_PORT || '8080';
+        this.httpsPort = process.env.APP_SSL_PORT || '8443';
+        try {
+            let privateKey  = fs.readFileSync(this.privateKeyFilePath, this.encoding);
+            let certificate = fs.readFileSync(this.certificateFilePath, this.encoding);
+
+            this.credentials = {key: privateKey, cert: certificate};
+        } catch (exception) {
+            this.credentials = null;
+        }
+    }
+
+    redirectToSSL() {
+        return (req, res, next) => {
+            if (this.credentials !== null && req.protocol === 'http') {
+                res.writeHead(301, { 'Location': `https://${req.get('host')}${req.url}` });
+                res.end();
+            } else {
+                next();
+            }
+        }
+    }
+
+}
